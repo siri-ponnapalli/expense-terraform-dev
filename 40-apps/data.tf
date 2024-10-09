93a8f8f35ed6e502340145b384bb9f3ae8d21b84
siri-ponnapalli <ponnapallisirichandana@gmail.com>
2024-10-09 19:35:30 +0530

    40-apps

--- 40-apps/data.tf
+++ 40-apps/data.tf
@@ -23,6 +23,11 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/Private_subnet_ids"
 }
 
+data "aws_ssm_parameter" "ansible_sg_id" {
+  name = "/${var.project_name}/${var.environment}/ansible_sg_id"
+}
+
+
 data "aws_ami" "joindevops" {
 	most_recent      = true
 	owners = ["973714476881"]
